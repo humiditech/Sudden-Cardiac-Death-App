1091f35985e2dc2730cb601938befa757eb4f884
aryawiratama18 <NAwiratama@gmail.com>
2021-07-28 01:49:47 +0700

    Relay Control OK

--- app/src/main/java/com/example/scdapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/scdapp/fragments/HomeFragment.java
@@ -15,6 +15,9 @@ import android.widget.TextView;
 
 import com.example.scdapp.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -22,6 +25,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
 
 public class HomeFragment extends Fragment {
 
@@ -34,6 +39,9 @@ public class HomeFragment extends Fragment {
     private FirebaseAuth fAuth;
     private FirebaseFirestore fStore;
     private String userId;
+    private FirebaseUser fUser;
+    private Button relayButton;
+    private int relayState = 0;
 
 
     @Override
@@ -43,6 +51,7 @@ public class HomeFragment extends Fragment {
         if (container == null) return null;
         RelativeLayout view = (RelativeLayout) inflater.inflate(R.layout.fragment_home, container, false);
         patientNickName = (TextView) view.findViewById(R.id.patient_home_name);
+        relayButton = (Button) view.findViewById(R.id.relay_button);
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         userId = fAuth.getCurrentUser().getUid();
@@ -54,6 +63,27 @@ public class HomeFragment extends Fragment {
                 patientNickName.setText(value.getString("nName"));
             }
         });
+
+        relayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (relayState == 0)
+                {
+                    relayState = 1;
+                    sendRelayCommand(relayState);
+                }
+                else{
+                    relayState = 0;
+                    sendRelayCommand(relayState);
+                }
+            }
+        });
         return view;
+    }
+
+    private void sendRelayCommand(final int relayState)
+    {
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Relay");
+        reference.child("relayState").setValue(relayState);
     }
 }
